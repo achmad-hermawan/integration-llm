be3a2d7c469356126df865cfa96d918be03b3d50
achmad <147478988+achmad-hermawan@users.noreply.github.com>
2025-12-23 11:07:01 +0700

    Delete check_models.py

--- check_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="API_KEY")
-
-models = genai.list_models()
-
-for m in models:
-    print(m.name, "=>", m.supported_generation_methods)
